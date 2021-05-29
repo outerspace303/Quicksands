c77cd8a0263d656a88b786c8710213bf24808f18
Jakub <49901045+outerspace303@users.noreply.github.com>
2021-05-29 23:09:37 +0200

    added tower, target locating

---
+++ Assets/Scripts/TargetLocator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] private Transform weapon;
+    private Transform target;
+
+    private void Start()
+    {
+        target = FindObjectOfType<EnemyMover>().transform;
+    }
+
+    private void Update()
+    {
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        weapon.transform.LookAt(target);
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private bool isPlaceable;
+    
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
+       
     }
 }
